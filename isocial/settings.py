7301f6e6ed32256b48e91cfffa4624b42fed35f5
Slim <mysleeem@gmail.com>
2024-06-24 00:32:28 +0200

    Railway error

--- isocial/settings.py
+++ isocial/settings.py
@@ -137,6 +137,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mysleeem@gmail.com'
-EMAIL_HOST_PASSWORD = 'sotuzpaydmyevgac'
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 EMAIL_USE_TLS = True
